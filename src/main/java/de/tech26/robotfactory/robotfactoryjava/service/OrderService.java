26bcdc4153ed7a5849c2545d5f6a0813e2f2c1b6
Geraldine Adelmann <gerakdine.adelmann@eldars.com.ar>
2021-12-08 20:18:54 -0300

    -made methods private -use of stream -add createOrderId()

--- src/main/java/de/tech26/robotfactory/robotfactoryjava/service/OrderService.java
+++ src/main/java/de/tech26/robotfactory/robotfactoryjava/service/OrderService.java
@@ -1,29 +1,42 @@
 package de.tech26.robotfactory.robotfactoryjava.service;
 
+import de.tech26.robotfactory.robotfactoryjava.repository.RobotPartRepository;
+import de.tech26.robotfactory.robotfactoryjava.entities.RobotPart;
 import de.tech26.robotfactory.robotfactoryjava.dtos.OrderRequest;
 import de.tech26.robotfactory.robotfactoryjava.dtos.OrderResponse;
 import de.tech26.robotfactory.robotfactoryjava.enums.TypeOfPartEnum;
-import de.tech26.robotfactory.robotfactoryjava.enums.RobotPartsEnum;
 import de.tech26.robotfactory.robotfactoryjava.exceptions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
 
-    int order_id;
+    private final RobotPartRepository robotPartRepository;
+
+
+    @Autowired
+    public OrderService(RobotPartRepository robotPartRepository) {
+        this.robotPartRepository = robotPartRepository;
+    }
 
     public OrderResponse createOrder(OrderRequest orderRequest) {
-        List<RobotPartsEnum> robotPartsList = orderRequest.getComponents();
+        List<RobotPart> robotPartsList = robotPartRepository.findPartsInOrderRequest(orderRequest);
         validateOrder(robotPartsList);
         double totalPrice = calculateTotalPrice(robotPartsList);
         updateStock(robotPartsList);
-        return new OrderResponse(this.order_id++, totalPrice);
+        return new OrderResponse(createOrderId(), totalPrice);
     }
 
-    public void validateOrder(List<RobotPartsEnum> robotPartsList) {
+    private String createOrderId(){
+        return  UUID.randomUUID().toString();
+    }
+
+    private void validateOrder(List<RobotPart> robotPartsList) {
         int amountOfRobotParts = robotPartsList.size();
         if (amountOfRobotParts < 4) {
             throw new NotEnoughPartsException("The order does not specify enough parts to build an entire robot");
@@ -32,29 +45,25 @@ public class OrderService {
             throw new MorePartsException("The order specifies more robot parts than needed");
         }
         List<TypeOfPartEnum> bodyPartsCheckList = new ArrayList<>();
-        for (RobotPartsEnum part : robotPartsList) {
+        for (RobotPart part : robotPartsList) {
             if (part.getStock() == 0) {
                 throw new RanOutOfStockException("We have unfortunately ran out of stock for " + part.getName());
             }
-            if (bodyPartsCheckList.contains(part.getPart())) {
+            if (bodyPartsCheckList.contains(part.getTypeOfPart())) {
                 throw new MoreThanOneOptionForATypeOfPartException("The order specifies more than one option for a body part. It should" +
                         " only contain ONE option for each part: face, material, arms and mobility ");
             } else {
-                bodyPartsCheckList.add(part.getPart());
+                bodyPartsCheckList.add(part.getTypeOfPart());
             }
         }
     }
 
-    public double calculateTotalPrice(List<RobotPartsEnum> robotPartsList) {
-        double totalPrice = 0;
-        for (RobotPartsEnum robotPart : robotPartsList) {
-            totalPrice += robotPart.getPrice();
-        }
-        return totalPrice;
+    private double calculateTotalPrice(List<RobotPart> robotPartsList) {
+        return robotPartsList.stream().mapToDouble(RobotPart::getPrice).sum();
     }
 
-    public void updateStock(List<RobotPartsEnum> robotPartsList){
-        for (RobotPartsEnum robotPart : robotPartsList) {
+    private void updateStock(List<RobotPart> robotPartsList){
+       for (RobotPart robotPart : robotPartsList) {
             int previousStock = robotPart.getStock();
             robotPart.setStock(previousStock - 1);
         }
